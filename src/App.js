74c6c994d7dd31268d6931fa75b68cffd943a7bd
안지수 <anjisu@anjisuui-MacBookPro.local>
2023-02-02 14:01:00 +0900

    버튼 영역 배치 완료

--- src/App.js
+++ src/App.js
@@ -2,102 +2,110 @@ import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { StyleSheet, Text, useWindowDimensions, View } from 'react-native';
 import Button, { ButtonTypes } from './components/Button';
-
 const Operators = {
-    CLEAR: 'C',
-    MINUS: '-',
-    PLUS: '+',
-    EQUAL: '=',
-  };
-  
-  export default function App() {
-    const [result, setResult] = useState(0);
-  
-    const width = (useWindowDimensions().width - 5) / 4;
+  CLEAR: 'C',
+  MINUS: '-',
+  PLUS: '+',
+  EQUAL: '=',
+};
+export default function App() {
+  const [result, setResult] = useState(0);
+  const width = (useWindowDimensions().width - 5) / 4;
+  return (
+    <View style={styles.container}>
+      <StatusBar style="light" />
+      <View style={styles.resultContainer}>
+        <Text style={styles.result}>{result}</Text>
+      </View>
 
-    return (
-      <View style={styles.container}>
-        <StatusBar style="light" />
-        <View style={styles.resultContainer}>
-          <Text style={styles.result}>{result}</Text>
-        </View>
-  
-        <View style={styles.buttonContainer}>
-          <View style={styles.leftPad}>
-            <View style={styles.number}></View>
-            <View style={styles.bottom}>
+      <View style={styles.buttonContainer}>
+        <View style={styles.leftPad}>
+          <View style={styles.number}>
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((num) => (
               <Button
-                title="0"
+                key={num}
+                title={num.toString()}
                 onPress={() => {}}
-                buttonStyle={{ width: width * 2, height: width }}
+                buttonStyle={{ width, height: width, marginTop: 1 }}
               />
-              <Button
-                title="="
-                onPress={() => {}}
-                buttonStyle={{ width, height: width }}
-                buttonType={ButtonTypes.OPERATOR}
-                />
-        </View>
-        </View>
+            ))}
+          </View>
 
-<View style={styles.operator}>
-  <Button
-    title={Operators.CLEAR}
-    onPress={() => {}}
-    buttonStyle={{ width, height: width, marginBottom: 1 }}
-    buttonType={ButtonTypes.OPERATOR}
-  />
-  <Button
-    title={Operators.MINUS}
-    onPress={() => {}}
-    buttonStyle={{ width, height: width, marginBottom: 1 }}
-    buttonType={ButtonTypes.OPERATOR}
-  />
-  <Button
-    title={Operators.PLUS}
-    onPress={() => {}}
-    buttonStyle={{ width, height: width * 2, marginBottom: 1 }}
-    buttonType={ButtonTypes.OPERATOR}
-  />
-</View>
-</View>
-</View>
-);
+          <View style={styles.bottom}>
+            <Button
+              title="0"
+              onPress={() => {}}
+              buttonStyle={{ width: width * 2, height: width }}
+            />
+            <Button
+              title="="
+              onPress={() => {}}
+              buttonStyle={{ width, height: width }}
+              buttonType={ButtonTypes.OPERATOR}
+            />
+          </View>
+        </View>
+        <View style={styles.operator}>
+          <Button
+            title={Operators.CLEAR}
+            onPress={() => {}}
+            buttonStyle={{ width, height: width, marginBottom: 1 }}
+            buttonType={ButtonTypes.OPERATOR}
+          />
+          <Button
+            title={Operators.MINUS}
+            onPress={() => {}}
+            buttonStyle={{ width, height: width, marginBottom: 1 }}
+            buttonType={ButtonTypes.OPERATOR}
+          />
+          <Button
+            title={Operators.PLUS}
+            onPress={() => {}}
+            buttonStyle={{ width, height: width * 2, marginBottom: 1 }}
+            buttonType={ButtonTypes.OPERATOR}
+          />
+        </View>
+      </View>
+    </View>
+  );
 }
-
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      flexDirection: 'column',
-      alignItems: 'stretch',
-      justifyContent: 'center',
-      backgroundColor: '#ffffff',
-    },
-    resultContainer: {
-      flex: 1,
-      justifyContent: 'flex-end',
-      alignItems: 'flex-end',
-      backgroundColor: '#000000',
-    },
-    buttonContainer: {
-      flexDirection: 'row',
-      backgroundColor: 'white',
-      justifyContent: 'space-evenly',
-    },
-    result: {
-        color: '#ffffff',
-        fontSize: 60,
-        fontWeight: '700',
-        paddingBottom: 30,
-        paddingRight: 30,
-      },
-    leftPad: {
-      width: '75%',
-    },
-    number: {},
-    bottom: {
-      flexDirection: 'row',
-      justifyContent: 'space-evenly',
-    },
-    operator: {},
-  });
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    backgroundColor: '#ffffff',
+  },
+  resultContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    backgroundColor: '#000000',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    backgroundColor: '#000000',
+    justifyContent: 'space-evenly',
+  },
+  result: {
+    color: '#ffffff',
+    fontSize: 60,
+    fontWeight: '700',
+    paddingBottom: 30,
+    paddingRight: 30,
+  },
+  leftPad: {
+    width: '75%',
+  },
+  number: {
+    flexDirection: 'row',
+    flexWrap: 'wrap-reverse',
+    justifyContent: 'space-evenly',
+  },
+  bottom: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+  operator: {},
+});
